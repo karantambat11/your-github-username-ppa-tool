07df0bddf21a7822f9e814d3280cdc31f23f2b3d
karantambat11 <karantambat11@gmail.com>
2025-04-29 15:47:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -216,7 +216,7 @@ if company_file and competitor_file:
             # After HTML render
             # Store dynamic HTML once on submit
             if 'matrix_html' not in st.session_state or submit_btn:
-                dynamic_html = generate_dynamic_html(sku_matrix, classification_metrics, tier_metrics, classifications, tiers)
+                dynamic_html = generate_dynamic_html(classifications, tiers)
                 st.session_state.matrix_html = dynamic_html
             
             # Always display cached HTML
